09433edabd24820f60b3710875d0f6073b95ec29
Shhatter <bartez378@gmail.com>
2016-07-14 21:22:31 +0200

    Popraweczki

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Gui.fxml"));
+        primaryStage.setTitle("To do application ");
+        primaryStage.setScene(new Scene(root, 700, 600));
         primaryStage.show();
     }
 
